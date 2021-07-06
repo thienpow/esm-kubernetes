a6a192a0d4f5aeb89cfbd211fa0e3cc09d0fe0b7
Ken Chong <thienpow@gmail.com>
2021-07-06 20:04:32 +0800

    minor fix

--- k8s-envoy/restart_frontenvoy.sh
+++ k8s-envoy/restart_frontenvoy.sh
@@ -4,7 +4,7 @@
 LD="$(../helper_sh/check_current_cluster.sh $1)"
 [[ "$(../helper_sh/ask_continue.sh ${LD})" =~ 9999 ]] && exit
 
-kubectl apply -f frontenvoy-configmap-$LD.yaml
-kubectl apply -f frontenvoy-deployment.yaml
+kubectl apply -f frontenvoy/frontenvoy-configmap-$LD.yaml
+kubectl apply -f frontenvoy/frontenvoy-deployment.yaml
 
 kubectl rollout restart deployment frontenvoy
--- k8s-envoy/restart_homeapp.sh
+++ k8s-envoy/restart_homeapp.sh
@@ -3,6 +3,6 @@
 
 LD="$(../helper_sh/check_current_cluster.sh $1)" # auto check and set the value for live or dev cluster.
 
-kubectl apply -f esm-homeapp-deployment-$LD.yaml
+kubectl apply -f homeapp/esm-homeapp-deployment-$LD.yaml
 
 kubectl rollout restart deployment esm-homeapp
