cbc80078b4810a8c2457cc4d4f2b7897dd4513c3
Ken Chong <thienpow@gmail.com>
2021-07-17 11:10:42 +0800

    add a 60s wait before continue restart pgpool

--- k8s-envoy/restart_all_pg.sh
+++ k8s-envoy/restart_all_pg.sh
@@ -2,8 +2,19 @@
 
 
 LD="$(../helper_sh/check_current_cluster.sh $1)" # auto check and set the value for live or dev cluster.
-#[[ "$(../helper_sh/ask_continue.sh ${LD})" =~ 9999 ]] && exit
 
 ./restart_pg-master.sh $LD
+
+wait_time=60
+echo "Wait for another ${wait_time} seconds"
+echo ""
+temp_cnt=${wait_time}
+while [[ ${temp_cnt} -gt 0 ]];
+do
+    printf "\rYou have %2d second(s) remaining!" ${temp_cnt}
+    sleep 1
+    ((temp_cnt--))
+done
+
 ./restart_pg-standby.sh $LD
 ./restart_pgpool.sh $LD
